13450d9fd81da98187c7fb1b85e79a91e346c638
Mathias Caipl <mathias.caipl@gmail.com>
2024-03-20 20:14:43 +0100

    Added storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook-vue/nuxt";
+import vue from '@vitejs/plugin-vue'
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook-vue/nuxt",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import('vite');
+
+    return mergeConfig(config, {
+      plugins: [vue()],
+    });
+  },
+};
+export default config;
